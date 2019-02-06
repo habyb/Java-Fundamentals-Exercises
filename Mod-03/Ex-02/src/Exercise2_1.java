099b8135d8a5d5665e59466b06476036d9d59631
Habyb.Fernandes <habybfernandes@gmail.com>
2019-02-06 10:54:40 -0200

    Added: Module 03 Ex2_1

---
+++ Mod-03/Ex-02/src/Exercise2_1.java
@@ -0,0 +1,10 @@
+public class Exercise2_1 {
+
+	public static void main(String[] args) {
+		
+		// Loop starts at 10 and ends at 25
+		for (int i = 10; i <= 25; i++) {
+			System.out.println(i);
+		}
+	}	
+}
